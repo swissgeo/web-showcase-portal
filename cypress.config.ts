e43cf51e3930857de10319456dfb9ae176977fda
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-04-28 17:46:16 +0200

    GPS-59 Clean up the example cypress testing facility

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {
-    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
-  },
+    e2e: {
+        specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+        baseUrl: 'http://localhost:4173',
+        chromeWebSecurity: false,
+        numTestsKeptInMemory: 1,
+    },
+    component: {
+        specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
+        devServer: {
+            framework: 'vue',
+            bundler: 'vite',
+        },
+    },
 })

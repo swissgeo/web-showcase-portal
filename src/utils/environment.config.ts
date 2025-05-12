eb55934d0b6c690612fbc392b724db304fd946d9
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2025-05-12 11:13:21 +0200

    GPS-62: allow use of int and dev mapviewer
    Issue: During development, we sometimes might want to use the dev mapviewer, to use features that are not yet deployed to production, or benefit from the latests bug fixes.
    
    Fix: We add environment handling, and add an environment variable which stores the URL for the map.

---
+++ src/utils/environment.config.ts
@@ -0,0 +1,26 @@
+/**
+ * File containing all environment variables handling.
+ */
+
+/**
+ * Object containing all environment variables.
+ *
+ * @type Object
+ */
+const environmentVariables: {[key: string]: string | number | boolean} = {
+    // mapviewer embed base URL, default to the prod
+    mapviewerEmbedUrl : import.meta.env.VITE_MAPVIEWER_BASE_EMBED_URL ?? 'https://map.geo.admin.ch/#embed/',
+}
+
+/**
+ * generic function to grab any environment variable
+ * @type String | null
+ */
+export function getEnvironmentVariable(key: string) {
+    return environmentVariables[key] ?? null
+}
+
+export function getEmbedViewerUrl() {
+    return environmentVariables.mapviewerEmbedUrl
+}
+

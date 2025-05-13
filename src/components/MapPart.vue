c38aff388994c2e4370d91d76e3832f38226522f
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-05-13 18:30:29 +0200

    GPS-53 Implement skeleton for the detail view

--- src/components/MapPart.vue
+++ src/components/MapPart.vue
@@ -1,19 +1,7 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-
-import DatasetInfo from '@/components/DatasetInfo.vue'
 import MapIntegration from '@/components/MapIntegration.vue'
-import { useMainStore } from '@/store/main'
-
-const mainStore = useMainStore()
-
-const { showLayerInfo } = storeToRefs(mainStore)
 </script>
 
 <template>
-    <MapIntegration
-        class="h-screen grow md:h-auto"
-        :class="{ 'md:hidden': showLayerInfo }"
-    ></MapIntegration>
-    <DatasetInfo v-if="showLayerInfo" />
+    <MapIntegration class="h-screen grow md:h-auto"></MapIntegration>
 </template>

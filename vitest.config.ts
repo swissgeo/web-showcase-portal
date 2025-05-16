0e039634189887813baabf7a9b48ef0f6bfc0524
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-05-16 11:48:11 +0200

    GPS-59 Disable dev tools in e2e tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,12 +4,12 @@ import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-    },
-  }),
+    viteConfig as object,
+    defineConfig({
+        test: {
+            environment: 'jsdom',
+            exclude: [...configDefaults.exclude, 'e2e/**'],
+            root: fileURLToPath(new URL('./', import.meta.url)),
+        },
+    })
 )

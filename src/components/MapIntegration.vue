acdf93d795ad941b67abb80f3d40c9ee49d00dc5
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2025-05-19 07:59:34 +0200

    GPS-61: adapt e2e tests

--- src/components/MapIntegration.vue
+++ src/components/MapIntegration.vue
@@ -34,15 +34,17 @@ function changeZoom(direction: boolean) {
         class="h-full w-full"
         allow="geolocation"
     ></iframe>
-    <ButtonGroup class="absolute right-2 bottom-38 md:right-6 md:bottom-8">
+    <ButtonGroup class="absolute right-2 bottom-38 md:right-6 md:bottom-8" data-cy="zoom-button-group">
         <Button
             :disabled="currentZoomLevel === 1"
+            data-cy="zoom-out"
             icon="pi pi-minus"
             severity="secondary"
             @click="changeZoom(false)"
         />
         <Button
             :disabled="currentZoomLevel === 15"
+            data-cy="zoom-in"
             icon="pi pi-plus"
             severity="secondary"
             @click="changeZoom(true)"

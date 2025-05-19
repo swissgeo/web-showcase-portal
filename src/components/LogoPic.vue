aa6ccc8802e9856c566502d3dfbae0d38cc318d6
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-05-19 16:55:37 +0200

    Add intermediary Logo

--- src/components/LogoPic.vue
+++ src/components/LogoPic.vue
@@ -8,21 +8,32 @@ const { condensed = false } = defineProps<{
     <!-- the desktop icon in the sidebar -->
     <div
         v-if="condensed"
-        class="flex h-12 w-full items-center justify-center blur-xs"
+        class="flex h-12 w-full items-center justify-center"
     >
         <img
-            src="@/assets/images/swissgeo_rgb_icon.svg"
+            src="@/assets/images/pwip-logo.png"
             class="h-5"
         />
+        <!-- <img
+            src="@/assets/images/swissgeo_rgb_icon.svg"
+            class="h-5"
+        /> -->
     </div>
     <!-- the mobile one-->
     <div
         v-else
-        class="flex items-center rounded border border-neutral-200 bg-white p-2 blur-xs md:rounded-none md:border-none"
+        class="flex items-center rounded border border-neutral-200 bg-white p-2 md:rounded-none md:border-none"
     >
-        <img
+        <div class="flex w-12 items-center justify-center">
+            <img
+                src="@/assets/images/pwip-logo.png"
+                class="h-5"
+            />
+            <!-- <img
             src="@/assets/images/swissgeo_rgb_sek.svg"
             class="h-5"
-        />
+        /> -->
+        </div>
+        <div class="text-primary-500 font-extrabold">GPS</div>
     </div>
 </template>

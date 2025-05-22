fd32b6120774b9606129c65a369f4faac7a21280
Ismail Sunni <imajimatika@gmail.com>
2025-05-22 11:42:39 +0700

    GPS-67: Add layer legend.

--- src/components/LayerLegendEntry.vue
+++ src/components/LayerLegendEntry.vue
@@ -2,21 +2,65 @@
 import AccordionContent from 'primevue/accordioncontent'
 import AccordionHeader from 'primevue/accordionheader'
 import AccordionPanel from 'primevue/accordionpanel'
+import { computed } from 'vue'
 
 import type { Layer } from '@/types/Layer'
 
 const { layer } = defineProps<{
     layer: Layer
 }>()
+
+const legendUrls = computed(() => {
+    const onlineResources = layer.geonetworkRecord?.onlineResources || []
+    const serviceLinks = onlineResources
+        .filter((resource) => {
+            const protocol = resource.accessServiceProtocol?.toLowerCase()
+            return protocol === 'wms'
+        })
+        .map((resource) => {
+            try {
+                const baseUrl = resource.url
+                const serviceUrl = new URL(baseUrl)
+                const legendUrl = new URL(baseUrl)
+
+                // Set parameters for GetLegendGraphic
+                legendUrl.search = new URLSearchParams({
+                    SERVICE: resource.accessServiceProtocol || 'WMS',
+                    REQUEST: 'GetLegendGraphic',
+                    VERSION: '1.3.0',
+                    FORMAT: 'image/png',
+                    LAYER: resource.name,
+                    // STYLE: 'default', commented out as not all services support this
+                    SLD_VERSION: '1.1.0',
+                }).toString()
+
+                return {
+                    url: serviceUrl.toString(),
+                    name: resource.name || 'No title',
+                    protocol: resource.accessServiceProtocol?.toUpperCase() || 'WMS',
+                    legendUrl: legendUrl.toString(),
+                }
+            } catch (e) {
+                // eslint-disable-next-line no-console
+                console.error(`Error processing service ${resource.url}: ${e}`)
+                return null
+            }
+        })
+        .filter(Boolean)
+
+    return serviceLinks
+})
 </script>
 
 <template>
     <AccordionPanel :value="layer.id">
         <AccordionHeader>{{ layer.name }}</AccordionHeader>
         <AccordionContent>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dolor doloremque
-            cupiditate recusandae sed explicabo accusamus vero? Asperiores, illo. Ea nobis natus
-            debitis commodi sed amet repellat iste neque quae.
+            <img
+                v-for="(service, index) in legendUrls"
+                :key="index"
+                :src="service?.legendUrl"
+            />
         </AccordionContent>
     </AccordionPanel>
 </template>

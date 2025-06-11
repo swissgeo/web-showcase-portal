8937742fccd448fa902aeac1829584fd7624c781
Sami Nouidri <sami.nouidri@swisstopo.ch>
2025-06-11 16:41:31 +0200

    GPS-148 : Fix search bar getting cut off on Firefox mobile

---
+++ src/utils/browser.ts
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * @returns True if the browser is Firefox on a mobile device
+ */
+export function isFirefoxMobile(): boolean {
+  const ua = navigator.userAgent
+  const isAndroidFirefox = /Firefox/i.test(ua) && /Android/i.test(ua)
+  const isiOSFirefox = /FxiOS/i.test(ua) && /(iPhone|iPad)/i.test(ua)
+
+  return isAndroidFirefox || isiOSFirefox
+}

16923b921915b83fdc751e8377bc286baf7201b2
Ismail Sunni <imajimatika@gmail.com>
2025-06-24 19:22:45 +0700

    GPS-71: Fix failed test.

--- src/components/GeocatalogButton.vue
+++ src/components/GeocatalogButton.vue
@@ -9,7 +9,7 @@ const isDesktop = inject<boolean>('isDesktop')
 // Access the store
 const uiStore = useUiStore()
 
-// Toggle visibility of the layer cart
+// Toggle visibility of the geocatalog tree
 const toggleGeocatalogTree = () => {
     uiStore.toggleGeocatalogTree()
 }
@@ -23,7 +23,7 @@ const toggleGeocatalogTree = () => {
             :outlined="!isDesktop"
             class="h-10"
             :class="{ 'h-14 w-14 rounded-xl bg-white': !isDesktop }"
-            data-cy="button-layer-cart"
+            data-cy="button-geocatalog-tree"
             @click="toggleGeocatalogTree"
         >
             <template #icon>

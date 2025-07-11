220486241883a8a9b9c93db67236309bb5aae15b
Ismail Sunni <imajimatika@gmail.com>
2025-07-11 21:40:52 +0700

    GPS-190: Add zoom out is disabled when zoom level is minimum.

--- cypress/e2e/map.spec.ts
+++ cypress/e2e/map.spec.ts
@@ -36,6 +36,20 @@ describe('Test the map on desktop', () => {
         getIframeDocument()
             .its('location.href')
             .should('match', /(?:\?|&)z=3/)
+
+        // Zoom out to minimum level (1) and check that zoom out button is disabled
+        cy.get('[data-cy="zoom-out"]').click()
+        getIframeDocument()
+            .its('location.href')
+            .should('match', /(?:\?|&)z=2/)
+
+        cy.get('[data-cy="zoom-out"]').click()
+        getIframeDocument()
+            .its('location.href')
+            .should('match', /(?:\?|&)z=1/)
+
+        // At zoom level 1 (minimum), the zoom out button should be disabled
+        cy.get('[data-cy="zoom-out"]').should('be.disabled')
     })
     it('loads a search result on the map', () => {
         cy.get('[data-cy="input-search"]').type('wald')

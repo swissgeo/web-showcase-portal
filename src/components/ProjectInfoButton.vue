86116d022aaba7fa9b89fd7e945034bd8cc2294a
Ismail Sunni <imajimatika@gmail.com>
2025-07-21 14:38:16 +0700

    GPS-189: Use translation for project information.

--- src/components/ProjectInfoButton.vue
+++ src/components/ProjectInfoButton.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Button from 'primevue/button'
 import { inject } from 'vue'
+import { useI18n } from 'vue-i18n'
 
+const { t } = useI18n()
 const isDesktop = inject<boolean>('isDesktop', true)
 
 function openProjectInfo() {
@@ -19,7 +21,7 @@ function openProjectInfo() {
         :outlined="!isDesktop"
         class="h-10"
         :class="{ 'h-14 w-14 rounded-xl bg-white': !isDesktop }"
-        title="Project Information"
+        :title="t('welcomeOverlay.projectInfoLink')"
         @click="openProjectInfo"
     >
         <template #icon>

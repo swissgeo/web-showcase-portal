ebb68ae05b04b243de64a130194585b00d3ec4a1
Ismail Sunni <imajimatika@gmail.com>
2025-08-11 15:56:39 +0700

    GPS-189: Update URL for EN and IT.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,5 +4,5 @@
 
 // External URLs
 export const PROJECT_INFO_URL = 'https://www.geoinformation.ch/de/swissgeo-geoplattform'
-export const PROJECT_INFO_URL_EN = 'https://www.geoinformation.ch/en/swissgeo-geoplatform'
-export const PROJECT_INFO_URL_IT = 'https://www.geoinformation.ch/it/swissgeo-geopiattaforma'
+export const PROJECT_INFO_URL_EN = 'https://www.geoinformation.ch/fr/swissgeo-geoplatform'
+export const PROJECT_INFO_URL_IT = 'https://www.geoinformation.ch/de/swissgeo-geopiattaforma'
